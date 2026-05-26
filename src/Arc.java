f2653d54077ca666b475f768b35148f6ef87eafc
Romain Wilhelm <romain57730@gmail.com>
2026-05-26 17:29:25 +0200

    Ajout de arc, arcs, graph

---
+++ src/Arc.java
@@ -0,0 +1,9 @@
+public class Arc {
+    String cible;
+    double poids;
+
+    public Arc(String cible, double poids) {
+        this.cible = cible;
+        this.poids = poids;
+    }
+}
---
+++ src/Arcs.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arcs {
+    private List<Arc> liste;
+
+    public Arcs() {
+        this.liste = new ArrayList<>();
+    }
+
+    public void ajouter(Arc a) {
+        liste.add(a);
+    }
+
+    public List<Arc> getListe() {
+        return liste;
+    }
+}
